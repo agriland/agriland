a17537b3ca3ff3644adabafb4c8a7b26902e003a
Rutger Broekhoff <rutger@viasalix.nl>
2020-11-09 20:26:57 +0000

    connecting

--- database.inc.php
+++ database.inc.php
@@ -5,7 +5,20 @@ function db_connect(): PDO
     include 'database_conninfo.inc.php';
 
     try {
-        return new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_database", "$db_username", "$db_password");
+        $conn = new PDO(
+            "mysql:host=$db_host;dbname=$db_database",
+            $db_username,
+            $db_password,
+            array(
+                PDO::MYSQL_ATTR_SSL_KEY   => './client-key.pem',
+                PDO::MYSQL_ATTR_SSL_CERT  => './client-cert.pem',
+                PDO::MYSQL_ATTR_SSL_CA    => './ca-cert.pem',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+            )
+        );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $conn;
     } catch (PDOException $e) {
         die("Could not connect to database: " . $e->getMessage());
     }
--- index.php
+++ index.php
@@ -14,6 +14,6 @@ $db_conn = db_connect();
     <title>Agriland</title>
 </head>
 <body>
-    
+    <p>Hallo</p>
 </body>
 </html>
