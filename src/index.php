2061762126ba64414b3fa9d03246177ded30aa65
Rutger Broekhoff <rutgerbroekhoff3@gmail.com>
2021-01-04 12:46:49 +0000

    wijzigingen

--- src/index.php
+++ src/index.php
@@ -9,83 +9,77 @@ $db_conn = new Database();
 
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agriland &middot; Home</title>
-    
-<link   href="style.css" rel="stylesheet" type="text/css"/>    
 
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="bulma.min.css" rel="stylesheet" type="text/css" />
+
+    <style>
+    img {
+        max-width: 380px;
+        float: centre;
+    }
+
+    html, body {
+        background-color: yellowgreen;
+        height: 100%;
+    }
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: center;
+        justify-content: space-evenly;
+        height: 100%;
+    }
+    </style>
 </head>
-<body style="background-color:yellowgreen;">
 
-    <img src="https://lh3.googleusercontent.com/TQkzD9v3lB0mh3j_T2jvZpyS9MbuSJrGMHneZLDc_UTotOBf-3sjGzQpG0vq73v1aD11=s85" alt="logo" class="Logo"/>     
-   
-<nav ALIGN="right">
-        <ul>
-            <li>
-                <a href="Inloggen">Inloggen</a>
-            </li>
-        </ul>
+<body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                <img src="agriland.jpg" width="28" height="28">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="/">Home</a>
+                <a class="navbar-item" href="/perceel.php">Perceel toevoegen</a>
+                <a class="navbar-item" href="/geteeldgewas.php">Gewas toevoegen</a>
+                <a class="navbar-item" href="">Over ons</a>
+                <a class="navbar-item" href="">Contact</a>
+            </div>
+        </div>
     </nav>
 
+    <div class="content">
 
+    <div>
+    Agriland staat voor overzicht en duurzaamheid. Op Agriland kunt u, door<br />
+    gebruik te maken van onze database, uw opbrengsten per gewas bijhouden.<br />
+    Hierdoor heeft u duidelijk inzicht in het rendement van uw gewassen.<br />
+    Ook is er de mogelijkheid om opbrengsten te vergelijken met verschillende jaren.<br />
+    Hierdoor kunt u streven naar de optimale winst van uw perceel.
+    </div>
+    
+    <div>
+        <img src="https://images1.persgroep.net/rcs/0Uaav2_L3cw4Fu__UnpjAA9wq5k/diocontent/131294043/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" />
+    </div>
 
-
-
-<h1 ALIGN="center" style="color:midnightblue;">AGRILAND</h1>    
-<hr/>
-<br/> <nav>
-        <ul>
-            <li>
-                <a href="" style="color:black;">Home</a>
-            </li>
-        </ul>
-    </nav>
-
-<nav>
-        <ul>
-            <li>
-                <a href="/perceel.php" style="color:black;">Perceel toevoegen</a>
-            </li>
-        </ul>
-    </nav>
-
-    <nav>
-        <ul>
-            <li>
-                <a href="/geteeldgewas.php" style="color:black;">Gewas toevoegen</a>
-            </li>
-        </ul>
-    </nav>
-
-
-<nav>
-        <ul>
-            <li>
-                <a href="" style="color:black;">Over ons</a>
-            </li>
-        </ul>
-    </nav>
-
-    <nav>
-        <ul>
-            <li>
-                <a href="" style="color:black;">Contact</a>
-            </li>
-        </ul>
-    </nav>
-    <br/>
-    <br/>
-   <hr/>
-<p ALIGN="center">Agriland staat voor overzicht en duurzaamheid. Op agriland kunt u, door <br/>gebruik te maken van onze database, uw opbrengsten per gewas bijhouden.<br/>Hierdoor heeft u duidelijk inzicht in het rendement van uw gewassen.<br/>Ook is er de mogelijkheid om opbrengsten te vergelijken met verschillende jaren.<br/>Hierdoor kunt u streven naar de optimale winst van uw perceel. 
-</p>
-<DIV ALIGN="center"><img src="https://images1.persgroep.net/rcs/0Uaav2_L3cw4Fu__UnpjAA9wq5k/diocontent/131294043/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8"/>
+    </div>
 </body>
+
 </html>
-<img src="Agriland.jpg">
-<style> 
-img {
-    max-width: 380px;
-    float:centre;
-}
