0bd0f3512f139f0e87a7a12743f85ab70046526a
Robert2307 <wieringa122@gmail.com>
2021-01-07 11:18:16 +0000

    werken aan over ons

--- src/overons.php
+++ src/overons.php
@@ -20,11 +20,22 @@
         height: 30%;
     }
 
+    .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+
+    }
+
     body {
         background-image: url('https://images.unsplash.com/photo-1497092801449-b782257c9756?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80');
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size: 100% 110%;
+    }
+    .text {
+        color:aliceblue;
     }
     </style>
 </head>
@@ -32,7 +43,24 @@
 <body>
 <?php include 'header.inc.php' ?>
 
-Deze site is gemaakt door:<br> Rutger Broekhoff, Tom van den Dorpel en Robert Wieringa<br/>
-<img src="Rutger.jpg"><img src="Tom.jpg"><img src="Robert.jpg">
-
+<div class="columns">
+  <div class="column">
+    <h1 class="is-size-2 has-text-centered"></br>Rutger Broekhoff</h1>
+    <img class="center" src="Rutger.jpg">
+    <p class=text> Rutger: Ik ben Rutger Broekhoff, klas 6 leerling aan het Calvijn College. 
+                   In mijn vrije tijd werk ik als software ontwikkelaar en DevOps-engineer bij Profex bv in Kapelle. 
+                   Ik heb ervaring met agri gerelateerde websites zoals Agro4all.nl.</p>
+  </div>
+  <div class="column">
+      <h1 class="is-size-2 has-text-centered">Deze site is gemaakt door:<br>
+      Tom van den Dorpel</h1><
+    <img class="center" src="Tom.jpg">
+  </div>
+  <div class="column">
+      <h1 class="is-size-2 has-text-centered"></br>Robert Wieringa</h1><br/>
+    <img class="center" src="Robert.jpg">
+    <p class=text> Robert: Ik ben Robert Wieringa, klas 6 leerling aan het Calvijn College. 
+        De reden om dit onderwerp te kiezen is mijn achtergrond, in mijn vrije tijd werk ik bij een boer. </p>
+  </div>
+</div>
 </body>
