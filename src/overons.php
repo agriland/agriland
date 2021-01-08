130a4b456762daf35eb44674e8d3a87fcc9a3c31
Robert2307 <wieringa122@gmail.com>
2021-01-08 09:09:14 +0000

    over ons fixen

--- src/overons.php
+++ src/overons.php
@@ -28,12 +28,6 @@
 
     }
 
-    body {
-        background-image: url('https://images.unsplash.com/photo-1497092801449-b782257c9756?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80');
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-size: 100% 110%;
-    }
     .text {
         color:aliceblue;
     }
@@ -45,19 +39,19 @@
 
 <div class="columns">
   <div class="column">
-    <h1 class="is-size-2 has-text-centered"></br>Rutger Broekhoff</h1>
+    <h1 class="is-size-3 has-text-centered"></br>Rutger Broekhoff</h1>
     <img class="center" src="Rutger.jpg">
     <p class=text> Rutger: Ik ben Rutger Broekhoff, klas 6 leerling aan het Calvijn College. 
                    In mijn vrije tijd werk ik als software ontwikkelaar en DevOps-engineer bij Profex bv in Kapelle. 
                    Ik heb ervaring met agri gerelateerde websites zoals Agro4all.nl.</p>
   </div>
   <div class="column">
-      <h1 class="is-size-2 has-text-centered">Deze site is gemaakt door:<br>
-      Tom van den Dorpel</h1><
+      <h1 class="is-size-3 has-text-centered">Deze site is gemaakt door:<br>
+      Tom van den Dorpel</h1>
     <img class="center" src="Tom.jpg">
   </div>
   <div class="column">
-      <h1 class="is-size-2 has-text-centered"></br>Robert Wieringa</h1><br/>
+      <h1 class="is-size-3 has-text-centered"></br>Robert Wieringa</h1><br/>
     <img class="center" src="Robert.jpg">
     <p class=text> Robert: Ik ben Robert Wieringa, klas 6 leerling aan het Calvijn College. 
         De reden om dit onderwerp te kiezen is mijn achtergrond, in mijn vrije tijd werk ik bij een boer. </p>
