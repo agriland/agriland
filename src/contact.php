6e8b716a9ecfcdd09705bc2e1bf3cf5204fa683f
Robert2307 <wieringa122@gmail.com>
2021-01-08 11:15:15 +0000

    contact

--- src/contact.php
+++ src/contact.php
@@ -29,7 +29,26 @@
 </head>
 
 <body>
-    <?php include 'header.inc.php' ?>
+    <style>
+        body {
+            background-image: url('https://images.unsplash.com/photo-1497092801449-b782257c9756?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 110%;
+        }
+    </style>
+    <?php include 'header.inc.php'?>
+    <div class="content">
+        <div>
+            <div>
+                <div>
+                    Voor vragen of opmerkingen over deze site kunt u mailen naar info@agriland.nl of bellen naar 06-12345678
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
 </body>
 
 </html>
